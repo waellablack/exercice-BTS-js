64133e8d1f270c28aedc4f1a60e860a9be002907
waellablack <waellablackpro@gmail.com>
2024-09-27 08:06:27 +0200

    Delete const bonjour = (nom) => {.js

--- const bonjour = (nom) => {.js
+++
@@ -1,8 +0,0 @@
-function convertir_temperature(celsius) {
-    let fahrenheit = (celsius * 9/5) + 32;
-    return fahrenheit;
-}
-
-
-let tempCelsius = 7;
-console.log(`La température en Fahrenheit est : ${convertir_temperature(tempCelsius)}`);
